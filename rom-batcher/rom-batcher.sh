557e753e367c3d5089505df2605b98e5de2105e9
Yogesh S <firzenyogesh@gmail.com>
2022-06-01 17:55:51 +0530

    readme

--- rom-batcher/rom-batcher.sh
+++ rom-batcher/rom-batcher.sh
@@ -13,18 +13,18 @@ askConfirmation() {
 extract() {
     echo "param ${1}"
     case "${1}" in
-    *.tar.bz2) tar xvjf $1 ;;
-    *.tar.gz) tar xvzf $1 ;;
-    *.bz2) bunzip2 $1 ;;
-    *.rar) rar x $1 ;;
-    *.gz) gunzip $1 ;;
-    *.tar) tar xvf $1 ;;
-    *.tbz2) tar xvjf $1 ;;
-    *.tgz) tar xvzf $1 ;;
+    # *.tar.bz2) tar xvjf $1 ;;
+    # *.tar.gz) tar xvzf $1 ;;
+    # *.bz2) bunzip2 $1 ;;
+    # *.rar) rar x $1 ;;
+    # *.gz) gunzip $1 ;;
+    # *.tar) tar xvf $1 ;;
+    # *.tbz2) tar xvjf $1 ;;
+    # *.tgz) tar xvzf $1 ;;
     *.zip) unzip -o -d "$(dirname "${1}")" "${1}" ;;
-    *.Z) uncompress $1 ;;
-    *.7z) 7z x $1 ;;
-    *) echo "don't know '$1'..." ;;
+    # *.Z) uncompress $1 ;;
+    # *.7z) 7z x $1 ;;
+    *) echo "Unsupported extraction format for ${1}" ;;
     esac
 }
 
